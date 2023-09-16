4e4682db70fbec5692bb77cfd8683e4f90f6de8f
Juan S. Marquerie <juansebastian.marquerie01@estudiant.upf.edu>
2023-09-16 13:42:54 +0200

    Fix submodule

--- sledgehammer/sl_utils.h
+++ sledgehammer/sl_utils.h
@@ -2,18 +2,20 @@
 #define __SL_UTILS_H__
 
 #include <stdio.h>
+#include <stdint.h>
 #include <assert.h>
 
 #define SL_TRUE 1
 #define SL_FALSE 0
+typedef uint8_t sl_bool;
 
 #define SL_EPSILON 0.001f
 
-inline static void assert_msg(const bool condition, 
-                       const char *msg) {
+inline static void assert_msg(const sl_bool condition, 
+                              const char *msg) {
     if (condition) {
         printf("ASERT ERROR %s", msg);
-        assert(false);
+        assert(SL_FALSE);
     }
 }
 
